a79878578996c0c1ebdd1a21cfd7bc82818b8a41
prometeodie <franco.d.r1992@gmail.com>
2022-12-29 17:53:35 -0300

    inserting a new hero component in process

--- src/app/heroes/pages/agregar/agregar.component.ts
+++ src/app/heroes/pages/agregar/agregar.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import { HeroesService } from '../../services/heroes.service';
+import { Heroe, Publisher } from '../heroe/interfaces/heroes.interface';
+
 @Component({
   selector: 'app-agregar',
   templateUrl: './agregar.component.html',
@@ -7,9 +10,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AgregarComponent implements OnInit {
 
-  constructor() { }
+  publishers = [
+    {
+      id: 'Dc Comics',
+      desc: 'Dc - Comics'
+    },
+    {
+      id: 'Marvel Comics',
+      desc: 'Marvel - Comics'
+    }
+  ];
+
+  heroe: Heroe = {
+    superhero: '',
+    alter_ego:'',
+    characters:'',
+    first_appearance:'',
+    publisher: Publisher.DCComics,
+    alt_img:'',
+  }
+  constructor(private heroesService: HeroesService) { }
 
   ngOnInit(): void {
+  }
+
+  SaveNewHero(){
+    
+    this.heroesService.setNewHero(this.heroe).subscribe()
+    
   }
 
 }
